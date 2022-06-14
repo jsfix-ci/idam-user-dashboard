318272141807bd12b7f0302867198d575c4aa7d7
“Thomas <32871802+Thomas-Geraghty@users.noreply.github.com>
2022-06-14 10:14:19 +0100

    SIDM-7454 - Removed idamAuth class and system user token caching

--- src/main/modules/awilix/index.ts
+++ src/main/modules/awilix/index.ts
@@ -14,8 +14,6 @@ import { LaunchDarkly } from '../../app/feature-flags/LaunchDarklyClient';
 const { Logger } = require('@hmcts/nodejs-logging');
 const logger = Logger.getLogger('app');
 import { defaultClient } from 'applicationinsights';
-import { IdamAuth } from '../../app/idam-auth/IdamAuth';
-import config from 'config';
 import { UserEditController } from '../../controllers/UserEditController';
 import { AccessibilityStatementController } from '../../controllers/AccessibilityStatementController';
 import { AddPrivateBetaServiceController } from '../../controllers/AddPrivateBetaServiceController';
@@ -44,29 +42,5 @@ export class Container {
       userSuspendController: asClass(UserSuspendController),
       accessibilityStatementController: asClass(AccessibilityStatementController)
     });
-
-    /**
-     * Function runs on container creation, gets system user token
-     * and creates new axios instance for use within DI.
-     * Refreshes token every 10mins if refresh failed, otherwise refreshes
-     * on half life of access token.
-     */
-    (function refreshSystemUser(): void {
-      const idamAuth = new IdamAuth(logger, defaultClient);
-      const { username, password } = config.get('services.idam.systemUser');
-      let delay = 10 * 60;
-
-      idamAuth.authorizePassword(username, password)
-        .then(({ tokens }) => {
-          app.locals.container.register({
-            systemAxios: asValue(idamAuth.getUserAxios(tokens.accessToken))
-          });
-
-          delay = tokens.accessToken.expires_in/2;
-          logger.info('Refreshed system user token. Refreshing again in: ' + delay/60 + 'mins');
-        })
-        .catch(() => logger.info('Failed to refresh system user token. Refreshing again in: ' + delay/60 + 'mins'))
-        .finally(() => setTimeout(refreshSystemUser, delay * 1000));
-    })();
   }
 }
